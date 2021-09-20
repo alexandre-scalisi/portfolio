c0803987b5b058b26af734886056523fec94cc31
alexlemia13 <alexlemia13@outlook.fr>
2021-09-20 23:16:31 +0200

    créé section compétences

--- js/nav.js
+++ js/nav.js
@@ -1,34 +1,48 @@
 const nav = () => {
   return {
-    data: [{
+    data: [
+      {
         text: "Présentation",
         img: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_Olympique_de_Marseille.svg/1200px-Logo_Olympique_de_Marseille.svg.png",
         active: true,
+        id: "presentation",
       },
       {
         text: "Compétences",
         img: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_Olympique_de_Marseille.svg/1200px-Logo_Olympique_de_Marseille.svg.png",
-        active: false
+        active: false,
+        id: "competences",
       },
       {
         text: "Projets",
         img: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_Olympique_de_Marseille.svg/1200px-Logo_Olympique_de_Marseille.svg.png",
-        active: false
+        active: false,
+        id: "projets",
       },
       {
         text: "Formations",
         img: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_Olympique_de_Marseille.svg/1200px-Logo_Olympique_de_Marseille.svg.png",
-        active: false
+        active: false,
+        id: "formations"
       },
       {
         text: "Contact",
         img: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_Olympique_de_Marseille.svg/1200px-Logo_Olympique_de_Marseille.svg.png",
-        active: false
+        active: false,
+        id: "contact",
       },
     ],
     active(name) {
-      this.data.forEach(dat => dat.text === name ? dat.active = true : dat.active = false);
+      this.data.forEach((dat) =>
+        dat.text === name ? (dat.active = true) : (dat.active = false)
+      );
     },
+
+    test(id) {
+      const targetOffset = document.getElementById(id).offsetTop;
+      console.log(document.getElementById(id));
+      window.scrollTo({ top: targetOffset, behavior: "smooth" });
+    }
   };
 
 };
---
+++ js/skills.js
@@ -0,0 +1,33 @@
+const skills = () => {
+  return {
+    languages: [
+      'js',
+      'html5',
+      'css3',
+      'php',
+      'ruby',
+      'csharp',
+      'SQL'
+    ],
+    frameworksOrLibraries: [
+      'Laravel',
+      'bootstrap',
+      'tailwind',
+      'NodeJS',
+      'Livewire',
+      'ExpressJS',
+      'Unity',
+      'Mongoose',
+      'Ruby On Rails',
+      'MongoDB'
+
+    ],
+    tools: [
+      'Git',
+      'Visual Studio Code',
+      'GitHub',
+      'NPM',
+      'Webpack'
+    ]
+  }
+}
