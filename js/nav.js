8e2a4bae599b6110f0935168a4a5b4364924bb8f
alexlemia13 <alexlemia13@outlook.fr>
2021-09-23 00:52:19 +0200

    reste le responsive, rajouter des projets et rendre le code plus propre

--- js/nav.js
+++ js/nav.js
@@ -3,33 +3,27 @@ const nav = () => {
     data: [
       {
         text: "Présentation",
-        img: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_Olympique_de_Marseille.svg/1200px-Logo_Olympique_de_Marseille.svg.png",
+        img: "../img/icons/user-icon.png",
         active: true,
         id: "presentation",
       },
       {
         text: "Compétences",
-        img: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_Olympique_de_Marseille.svg/1200px-Logo_Olympique_de_Marseille.svg.png",
+        img: "../img/icons/skills-icon.png",
         active: false,
         id: "competences",
       },
       {
         text: "Projets",
-        img: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_Olympique_de_Marseille.svg/1200px-Logo_Olympique_de_Marseille.svg.png",
+        img: "../img/icons/projects-icon.png",
         active: false,
         id: "projets",
       },
       {
         text: "Formations",
-        img: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_Olympique_de_Marseille.svg/1200px-Logo_Olympique_de_Marseille.svg.png",
+        img: "../img/icons/formations-icon.png",
         active: false,
-        id: "formations"
-      },
-      {
-        text: "Contact",
-        img: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_Olympique_de_Marseille.svg/1200px-Logo_Olympique_de_Marseille.svg.png",
-        active: false,
-        id: "contact",
+        id: "formations",
       },
     ],
     active(name) {
@@ -42,7 +36,7 @@ const nav = () => {
       const targetOffset = document.getElementById(id).offsetTop;
       console.log(document.getElementById(id));
       window.scrollTo({ top: targetOffset, behavior: "smooth" });
-    }
+    },
   };
 
 };
